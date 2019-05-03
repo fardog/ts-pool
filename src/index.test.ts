2bc2415558208dfe0c74d44de358bab3635d16ae
fardog <nate@milkandtang.com>
2019-05-03 09:12:49 -0700

    proof of concept

---
+++ src/index.test.ts
@@ -0,0 +1,101 @@
+import Pool, { Options, Deferred } from "./"
+import { expressionStatement } from "@babel/types"
+
+describe('utility test cases', () => {
+  test("deferred has reject/resolve immediately available", () => {
+    const deferred = new Deferred<number>();
+    expect(deferred.resolve).toBeTruthy()
+    expect(deferred.reject).toBeTruthy()
+  })
+})
+
+describe("basic test case", () => {
+  const options: Options = {
+    minResources: 0,
+    maxResources: 10
+  }
+  let createCount: number = 0
+  let disposeCount: number = 0
+  const disposed: jest.Mock<any, any>[] = []
+
+  class TestPool extends Pool<jest.Mock> {
+    create(): Promise<jest.Mock> {
+      createCount++
+      return Promise.resolve(jest.fn())
+    }
+    dispose(rsc: jest.Mock<any, any>): Promise<void> {
+      disposeCount++
+      disposed.push(rsc)
+      return Promise.resolve()
+    }
+  }
+
+  afterEach(() => {
+    createCount = 0
+    disposeCount = 0
+    disposed.splice(0, disposed.length)
+  })
+
+  test("borrow returns same resource after release", async () => {
+    const pool = new TestPool(options)
+
+    const [rsc, release] = await pool.borrow()
+    expect(rsc).toBeTruthy()
+    expect(rsc).toBeCalledTimes(0)
+
+    // call for later assertion, and release
+    rsc()
+    await release()
+
+    const [rsc2] = await pool.borrow()
+
+    expect(rsc2).toBeCalledTimes(1)
+    // verify is same resource
+    expect(rsc2).toBe(rsc)
+  })
+
+  test("borrow returns different resources", async () => {
+    const pool = new TestPool(options)
+
+    const [rsc1] = await pool.borrow()
+    const [rsc2] = await pool.borrow()
+
+    expect(rsc1).toBeTruthy()
+    expect(rsc2).toBeTruthy()
+    expect(rsc1).not.toBe(rsc2)
+  })
+
+  test("borrow cannot exceed cap", async () => {
+    expect.assertions(1)
+
+    const pool = new TestPool({ minResources: 0, maxResources: 2 })
+
+    const [rsc1, release1] = await pool.borrow()
+    const [rsc2, release2] = await pool.borrow()
+
+    pool.borrow().then(([rsc]) => {
+      expect(rsc).toBe(rsc1)
+    })
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        release1()
+        release2()
+        setTimeout(() => pool.destroy().then(() => resolve()), 10)
+      }, 10)
+    })
+  })
+
+  test("pool with minimum set allocates resources", async () => {
+    expect.assertions(1)
+
+    const pool = new TestPool({ minResources: 5, maxResources: 10 })
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(createCount).toEqual(5)
+        pool.destroy().then(() => resolve())
+      }, 10)
+    })
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,218 @@
+export interface Options {
+  minResources: number
+  maxResources: number
+
+  maxRequests?: number
+
+  acquireTimeout?: number
+  disposeTimeout?: number
+  borrowTimeout?: number
+  idleTimeout?: number
+
+  syncInterval?: number
+  resourceMaxAge?: number
+}
+
+export type Borrowed<T> = [T, () => void]
+
+interface ObjectInfo {
+  created: number
+}
+
+export class Deferred<T> {
+  public promise: Promise<T>
+  public resolve?: (value: T) => void
+  public reject?: (reason: any) => void
+
+  constructor() {
+    this.promise = new Promise<T>((resolve, reject) => {
+      this.resolve = resolve
+      this.reject = reject
+    })
+  }
+}
+
+abstract class Pool<T> {
+  protected options: Options
+  private ending: boolean = false
+
+  private known: Map<T, ObjectInfo> = new Map()
+  private available: Array<T> = []
+  private queue: Array<Deferred<Borrowed<T>>> = []
+
+  private syncTimeout?: number
+
+  constructor(opts: Options) {
+    this.options = opts
+
+    if (opts.syncInterval) {
+      this.syncTimeout = setInterval(() => {
+        this.sync()
+      }, opts.syncInterval)
+    }
+
+    // start pool
+    this.sync()
+  }
+
+  abstract create(): Promise<T>
+  abstract dispose(rsc: T): Promise<void>
+
+  public borrow = async (): Promise<Borrowed<T>> => {
+    if (this.ending) {
+      throw new Error("pool is ending")
+    }
+
+    const rsc = this.available.shift()
+    if (rsc && this.resourceIsExpired(rsc)) {
+      this.removeResource(rsc)
+    } else if (rsc) {
+      const val: Borrowed<T> = [rsc, () => this.returnResource(rsc)]
+
+      return Promise.resolve(val)
+    }
+
+    const future = new Deferred<Borrowed<T>>()
+    this.queue.push(future)
+
+    // sync pool, which may add a resource for this request; or it may reject it
+    // for being over the queue limit. don't wait for that here, just sync and
+    // return the promise to the caller
+    this.sync()
+
+    return future.promise
+  }
+
+  public remove = async (rsc: T): Promise<void> => {
+    return this.removeResource(rsc)
+  }
+
+  public destroy = async (): Promise<void> => {
+    this.ending = true
+
+    // stop pool sync, if it's doing so on interval
+    if (this.syncTimeout) {
+      clearTimeout(this.syncTimeout)
+    }
+
+    // reject all queued requests
+    this.queue
+      .splice(0, this.queue.length)
+      .forEach(p => p.reject && p.reject(new Error("pool is ending")))
+
+    // drain available pool
+    const promises = this.available
+      .splice(0, this.available.length)
+      .map(this.removeResource)
+
+    return Promise.all(promises).then(() => { })
+  }
+
+  private addResource = async (rsc: T): Promise<void> => {
+    if (this.known.get(rsc)) {
+      throw new Error(
+        "addResource called on a resource that already exists in the pool"
+      )
+    }
+    const info: ObjectInfo = {
+      created: Date.now()
+    }
+    this.known.set(rsc, info)
+
+    return this.maybeLendResource(rsc)
+  }
+
+  private maybeLendResource = async (rsc: T): Promise<void> => {
+    // see if there's a waiting borrower that can use the resource
+    const deferred = this.queue.shift()
+
+    // if not, put it back in the pool
+    if (!deferred) {
+      this.available.push(rsc)
+      return
+    }
+
+    // lend the resource
+    const borrow: Borrowed<T> = [rsc, () => this.returnResource(rsc)]
+    if (deferred.resolve) {
+      return deferred.resolve(borrow)
+    }
+  }
+
+  private returnResource = async (rsc: T): Promise<void> => {
+    const info = this.known.get(rsc)
+    if (!info) {
+      throw new Error("return resource called for unknown object")
+    }
+
+    const { resourceMaxAge } = this.options
+
+    // dispose of the resource if it's aged out
+    if (resourceMaxAge && Date.now() - info.created > resourceMaxAge) {
+      return this.removeResource(rsc)
+    }
+
+    return this.maybeLendResource(rsc)
+  }
+
+  private removeResource = (rsc: T): Promise<void> => {
+    this.known.delete(rsc)
+    const idx = this.available.indexOf(rsc)
+    if (idx > -1) {
+      this.available.slice(idx, 1)
+    }
+
+    return this.dispose(rsc)
+  }
+
+  private resourceIsExpired = (rsc: T): boolean => {
+    const { resourceMaxAge = Infinity } = this.options
+    const info = this.known.get(rsc)
+    if (!info) {
+      throw new Error("object is unknown to this pool")
+    }
+
+    return Date.now() - info.created > resourceMaxAge
+  }
+
+  private sync = async (): Promise<void> => {
+    const promises: Promise<any>[] = []
+    const { maxResources, minResources, maxRequests = Infinity } = this.options
+
+    // expire out of date resources
+    this.available = this.available.slice().filter(rsc => {
+      if (!this.resourceIsExpired(rsc)) {
+        promises.push(this.removeResource(rsc))
+        return
+      }
+
+      return true
+    })
+
+    // grow resource pool if allowed/necessary
+    const currentSize = this.known.size
+    const deficit = this.queue.length
+    const requestedSize = Math.max(
+      minResources,
+      Math.min(currentSize + deficit, maxResources)
+    )
+    const toCreate = Math.max(requestedSize - currentSize, 0)
+
+    for (let i = 0; i < toCreate; ++i) {
+      promises.push(this.create().then(rsc => this.addResource(rsc)))
+    }
+
+    // reject any queued requests over the queue limit
+    if (this.queue.length > maxRequests) {
+      this.queue
+        .splice(maxRequests, this.queue.length - maxRequests)
+        .forEach(future => {
+          if (future.reject) future.reject(new Error("queue length exceeded"))
+        })
+    }
+
+    return Promise.all(promises).then(() => { })
+  }
+}
+
+export default Pool
